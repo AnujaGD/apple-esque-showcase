2342bf059b514c4b83a42ca24d1c3c23ebdff66e
AnujaGD <anujagdhole@gmail.com>
2025-05-01 15:37:53 +0530

    add

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -23,7 +23,7 @@ const Hero: React.FC = () => {
           <a href="#contact" className="apple-button-secondary">
             Get in Touch
           </a>
-          <a href="resume.pdf"  className="apple-button-secondary" >
+          <a href="resume.pdf"  className="apple-button-secondary" download="resume.pdf" >
             Download Resume
           </a>
         </div>
